c16294b078e1d36501efd2aa4664e03c9cf5b95f
marquesmoreirasousa <marques.sousa@ifsp.edu.br>
2023-03-16 22:32:11 -0300

    Adição de edição e listagem dinâmica de clientes.

--- PizzariaIFSP/Cliente.cs
+++ PizzariaIFSP/Cliente.cs
@@ -8,36 +8,67 @@ namespace PizzariaIFSP
 {
     internal class Cliente
     {
-        private string Nome;
-        private string Telefone;
-        private string Email;
-        private DateTime DatNasc;
+        private int id;
+        private string nome;
+        private string telefone;
+        private string email;
+        private DateTime datNasc;
 
-        public Cliente(string nome, string tel)
+        public Cliente(int id, string nome, string tel)
         {
+            Id = id;
             Nome = nome;
-            Telefone = tel; 
+            Telefone = tel;
         }
-        public Cliente(string nome, string tel, string email, DateTime datNasc)
+        public Cliente(int id, string nome, string tel, string email, DateTime datNasc)
         {
+            Id = id;
             Nome = nome;
             Telefone = tel;
             Email = email;
             DatNasc = datNasc;
         }
-
-        public String telefone
+        public int Id
         {
-            get { return Telefone; }
+            get { return id; }
+            set { id = value; }
+        }
+        public String Telefone
+        {
+            get { return telefone; }
+            set { 
+                if(value != String.Empty)
+                    telefone = value; 
+            }
+        }
+        public String Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+        public String Email
+        {
+            get { return email; }
+            set { email = value; }
         }
 
+        public DateTime DatNasc
+        {
+            get { return datNasc; }
+            set { datNasc = value; }
+        }
         public override string ToString()
         {
-            return
-                "\nNOME: " + Nome +
-                "\nTELEFONE:" + Telefone +
-                "\nEMAIL:" + Email +
-                "\nNASCIMENTO: " + DatNasc.ToShortDateString();
+            string saida = "\nID: " + Id +
+                           "\nNOME: " + Nome +
+                           "\nTELEFONE:" + Telefone;
+            if (!String.IsNullOrEmpty(Email))
+                saida = saida + "\nEMAIL:" + Email;
+            if(!(DatNasc.CompareTo(Convert.ToDateTime("01/01/0001") ) == 0))
+            //if(DatNasc.ToShortDateString() == Convert.ToDateTime("01/01/0001").ToString())
+                saida = saida + "\nNASCIMENTO: " + DatNasc.ToShortDateString();
+
+            return saida;
         }
     }
 }
--- PizzariaIFSP/Program.cs
+++ PizzariaIFSP/Program.cs
@@ -10,16 +10,18 @@ namespace PizzariaIFSP
     {
         static int Menu()
         {
-            Console.WriteLine("SAIR ------ 0");
-            Console.WriteLine("CADASTRAR - 1");
-            Console.WriteLine("LISTAR ---- 2");
-            Console.WriteLine("REMOVER --- 3");
-            Console.WriteLine("EDITAR ---- 4");
+            Console.WriteLine("\nSAIR -------- 0");
+            Console.WriteLine("CADASTRAR --- 1");
+            Console.WriteLine("LISTAR ------ 2");
+            Console.WriteLine("EDITAR ------ 3");
+            Console.WriteLine("REMOVER ----- 4");
+            Console.WriteLine("PESQUISAR --- 5");
             return int.Parse(Console.ReadLine());
         }
         static void Main(string[] args)
         {
             int qtd = 0, opcao;
+            string telefone, nome;
             Cliente[] vetCli = new Cliente[100];
             do {
                 opcao = Menu();
@@ -30,9 +32,9 @@ namespace PizzariaIFSP
                         break;
                     case 1:
                         Console.WriteLine("Nome: ");
-                        string nome = Console.ReadLine();
+                        nome = Console.ReadLine();
                         Console.WriteLine("Telefone: ");
-                        string telefone = Console.ReadLine();
+                        telefone = Console.ReadLine();
                         Console.WriteLine("Quer preencher e-mail " +
                             "e data de nascimento? 1 - Sim ou 0 - Não");
                         int escolha = int.Parse(Console.ReadLine());
@@ -43,12 +45,12 @@ namespace PizzariaIFSP
                             Console.WriteLine("Data de nascimento: ");
                             DateTime nasc = Convert.ToDateTime(Console.ReadLine());
                             vetCli[qtd++] = new Cliente(
-                                nome, telefone, email, nasc);
+                                qtd, nome, telefone, email, nasc);
                         }
                         else
                         {
                             vetCli[qtd++] = new Cliente(
-                                nome, telefone);
+                                qtd, nome, telefone);
                         }
                         break;
                     case 2:
@@ -57,12 +59,59 @@ namespace PizzariaIFSP
                             Console.WriteLine(vetCli[i].ToString());
                         }
                         break;
-                    case 4:
-                        Console.WriteLine("Digite o número de telefone: ");
-                        string tel = Console.ReadLine();
+                    case 3:
+                        Console.WriteLine("Entre com o telefone a ser pesquisado: ");
+                        telefone = Console.ReadLine();
                         for (int i = 0; i < qtd; i++)
                         {
-                            if (vetCli[i].telefone == tel)
+                            if (vetCli[i].Telefone == telefone)
+                            {
+                                int op;
+                                do
+                                {
+                                    Console.WriteLine("Estes são os dados do cliente: ");
+                                    Console.WriteLine(vetCli[i].ToString());
+                                    Console.WriteLine("======= OPÇÕES =======");
+                                    Console.WriteLine("    SAIR DA EDIÇÃO - 0");
+                                    Console.WriteLine("       EDITAR NOME - 1");
+                                    Console.WriteLine("   EDITAR TELEFONE - 2");
+                                    Console.WriteLine("     EDITAR E-MAIL - 3");
+                                    Console.WriteLine("       EDITAR DATA - 4");
+                                    op = int.Parse(Console.ReadLine());
+                                    switch (op)
+                                    {
+                                        case 1:
+                                            Console.WriteLine("Nome: ");
+                                            vetCli[i].Nome = Console.ReadLine();
+                                            break;
+                                        case 2:
+                                            Console.WriteLine("Telefone: ");
+                                            vetCli[i].Telefone = Console.ReadLine();
+                                            break;
+                                        case 3:
+                                            Console.WriteLine("E-mail: ");
+                                            vetCli[i].Email = Console.ReadLine();
+                                            break;
+                                        case 4:
+                                            Console.WriteLine("Data de nascimento: ");
+                                            vetCli[i].DatNasc = Convert.ToDateTime(Console.ReadLine());
+                                            break;
+                                        default:
+                                            Console.WriteLine("Opção inválida!" +
+                                                "\nCliente não atualizado!");
+                                            break;
+                                    }
+                                } while (op != 0);
+                                break;
+                            }
+                        }
+                        break;
+                    case 4:
+                        Console.WriteLine("Digite o número de telefone: ");
+                        telefone = Console.ReadLine();
+                        for (int i = 0; i < qtd; i++)
+                        {
+                            if (vetCli[i].Telefone == telefone)
                             {
                                 vetCli[i] = vetCli[qtd - 1];
                                 qtd--;
@@ -70,6 +119,31 @@ namespace PizzariaIFSP
                             }
                         }
                         break;
+                    case 5:
+                        string digitou = String.Empty;
+
+                        Console.WriteLine("Digite algo para pesquisar...");
+                        ConsoleKeyInfo simb = Console.ReadKey();
+                        while (simb.Key != ConsoleKey.Enter)
+                        {
+                            digitou = digitou + simb.KeyChar;
+                            Console.Clear();
+                            Console.WriteLine("Resultados: ");
+
+                            for (int i = 0; i < qtd; i++)
+                            {
+                                if (vetCli[i].Nome.ToUpper().Contains(digitou.ToUpper()))
+                                {
+                                    Console.WriteLine(vetCli[i].ToString());
+                                    Console.WriteLine("---------------");
+                                }
+                            }
+                            Console.WriteLine("\nNome procurado:" + digitou);
+                            Console.WriteLine("Digite enter para encerrar...");
+                            simb = Console.ReadKey();
+                        }
+
+                            break;
                     default:
                         Console.WriteLine("Opção inválida!");
                         break;
